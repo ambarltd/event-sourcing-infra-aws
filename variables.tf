83aaf5db24df6826c471c03244cd08fae0316ed2
Tom Schutte <tom@ambar.cloud>
2025-07-16 16:51:57 -0500

    Correct type

--- variables.tf
+++ variables.tf
@@ -40,7 +40,7 @@ variable "mongodbatlas_project_id" {
 
 variable "mongodbatlas_free_tier" {
   description = "If the projection store should use the M0 or M10 cluster size"
-  type = boolean
+  type = bool
   default = false
 }
 
