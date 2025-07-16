909f900b3087c89069ccb8e19101137671423ec3
Tom Schutte <tom@ambar.cloud>
2025-07-16 14:54:23 -0500

    Add free_tier to outputs

--- terraform/projection_store/outputs.tf
+++ terraform/projection_store/outputs.tf
@@ -1,20 +1,24 @@
 output "connection_string" {
   description = "The connection string for the MongoDB Atlas cluster"
-  value       = mongodbatlas_cluster.projection_store.connection_strings[0].standard
+  value       = var.mongodb_free_tier ? mongodbatlas_cluster.free_projection_store[0].connection_strings[0].standard : mongodbatlas_cluster.projection_store[0].connection_strings[0].standard
   sensitive   = true
 }
 
 output "srv_connection_string" {
   description = "The SRV connection string for the MongoDB Atlas cluster"
-  value       = mongodbatlas_cluster.projection_store.connection_strings[0].standard_srv
+  value       = var.mongodb_free_tier ? mongodbatlas_cluster.free_projection_store[0].connection_strings[0].standard_srv : mongodbatlas_cluster.projection_store[0].connection_strings[0].standard_srv
   sensitive   = true
 }
 
 output "srv_connection_host" {
   description = "The SRV connection string for the MongoDB Atlas cluster"
   # remove any portion of the values that are supplied later on.
-  value = replace(
-    mongodbatlas_cluster.projection_store.connection_strings[0].standard_srv,
+  value = var.mongodb_free_tier ? replace(
+    mongodbatlas_cluster.free_projection_store[0].connection_strings[0].standard_srv,
+    "/^mongodb\\+srv:\\/\\//",
+    ""
+  ) : replace(
+    mongodbatlas_cluster.projection_store[0].connection_strings[0].standard_srv,
     "/^mongodb\\+srv:\\/\\//",
     ""
   )
@@ -23,12 +27,12 @@ output "srv_connection_host" {
 
 output "cluster_id" {
   description = "The ID of the MongoDB Atlas cluster"
-  value       = mongodbatlas_cluster.projection_store.cluster_id
+  value       = var.mongodb_free_tier ? mongodbatlas_cluster.free_projection_store[0].cluster_id :  mongodbatlas_cluster.projection_store[0].cluster_id
 }
 
 output "cluster_name" {
   description = "The name of the MongoDB Atlas cluster"
-  value       = mongodbatlas_cluster.projection_store.name
+  value       = var.mongodb_free_tier ? mongodbatlas_cluster.free_projection_store[0].name : mongodbatlas_cluster.projection_store[0].name
 }
 
 output "projection_store_user" {
