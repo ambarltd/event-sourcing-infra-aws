13bb9126897a4b16dd2dce52f3d2ae9a45fb40f2
Tom Schutte <tom@ambar.cloud>
2025-07-18 18:30:57 -0500

    Remove providers

--- providers.tf
+++ providers.tf
@@ -22,21 +22,22 @@ terraform {
 }
 
 # We deploy resources in AWS to a customer application account, so we configure the provider with an IAM user AK + SK
-provider "aws" {
-  region = var.region
-  # https://developer.hashicorp.com/terraform/language/settings/backends/configuration#partial-configuration
-  # https://discuss.hashicorp.com/t/error-invalid-aws-region-ap-southeast-4/49985/2
-  skip_region_validation = true
-  access_key = var.application_account_aws_access_key
-  secret_key = var.application_account_aws_secret_key
-}
-
-provider "mongodbatlas" {
-  public_key  = var.mongodbatlas_public_key
-  private_key = var.mongodbatlas_private_key
-}
-
-provider "ambar" {
-  endpoint = var.ambar_regional_endpoint
-  api_key  = var.ambar_api_key
-}
+# Providers are passed down from calling templates
+#provider "aws" {
+#  region = var.region
+#  # https://developer.hashicorp.com/terraform/language/settings/backends/configuration#partial-configuration
+#  # https://discuss.hashicorp.com/t/error-invalid-aws-region-ap-southeast-4/49985/2
+#  skip_region_validation = true
+#  access_key = var.application_account_aws_access_key
+#  secret_key = var.application_account_aws_secret_key
+#}
+#
+#provider "mongodbatlas" {
+#  public_key  = var.mongodbatlas_public_key
+#  private_key = var.mongodbatlas_private_key
+#}
+#
+#provider "ambar" {
+#  endpoint = var.ambar_regional_endpoint
+#  api_key  = var.ambar_api_key
+#}
