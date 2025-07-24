44a65d623b502d2c01b421e5693c84d521759484
Tom Schutte <tom@ambar.cloud>
2025-07-24 12:06:40 -0500

    Make sure IAM uses the same provider

--- terraform/email/iam.tf
+++ terraform/email/iam.tf
@@ -1,9 +1,13 @@
 # IAM Policy for SES Email Sending
 resource "aws_iam_user" "smtp_user" {
+  provider       = aws.alt_region
+
   name = "${var.environment_name}-smtp-user"
 }
 
 resource "aws_iam_access_key" "smtp_user" {
+  provider       = aws.alt_region
+
   user = aws_iam_user.smtp_user.name
 }
 
@@ -15,12 +19,16 @@ data "aws_iam_policy_document" "ses_sender" {
 }
 
 resource "aws_iam_policy" "ses_sender" {
+  provider       = aws.alt_region
+
   name        = "ses_sender"
   description = "Allows sending of e-mails via Simple Email Service"
   policy      = data.aws_iam_policy_document.ses_sender.json
 }
 
 resource "aws_iam_user_policy_attachment" "test-attach" {
+  provider       = aws.alt_region
+
   user       = aws_iam_user.smtp_user.name
   policy_arn = aws_iam_policy.ses_sender.arn
 }
