1cc2b19105c66c919ff36d02b99f8133b33d02de
Tom Schutte <tom@ambar.cloud>
2025-07-24 08:15:46 -0500

    Remove email resources for now

--- main.tf
+++ main.tf
@@ -5,19 +5,19 @@ locals {
 
 
 # Email Module
-module "email" {
-  source = "./terraform/email"
-
-  environment_name       = var.environment_name
-  domain_name            = var.top_level_domain
-  route53_zone_name      = var.hosted_zone_name
-  route53_zone_id        = var.hosted_zone_id
-  allowed_from_addresses = ["${var.from_email}@${var.top_level_domain}"]
-
-  providers = {
-    aws = aws.ses
-  }
-}
+#module "email" {
+#  source = "./terraform/email"
+#
+#  environment_name       = var.environment_name
+#  domain_name            = var.top_level_domain
+#  route53_zone_name      = var.hosted_zone_name
+#  route53_zone_id        = var.hosted_zone_id
+#  allowed_from_addresses = ["${var.from_email}@${var.top_level_domain}"]
+#
+##  providers = {
+##    aws = aws.ses
+##  }
+#}
 
 # Network Module
 module "network" {
@@ -155,10 +155,10 @@ module "backend_container_service" {
   mongodb_password = module.projection_store.projection_store_password
 
   # SMTP Configuration
-  smtp_host       = module.email.smtp_host
-  smtp_port       = module.email.smtp_port
-  smtp_username   = module.email.smtp_username
-  smtp_password   = module.email.smtp_password
+  smtp_host       = "" #module.email.smtp_host
+  smtp_port       = "" #module.email.smtp_port
+  smtp_username   = "" #module.email.smtp_username
+  smtp_password   = "" #module.email.smtp_password
   smtp_from_email = "${var.from_email}@${var.top_level_domain}"
 
   desired_count = var.backend_instance_count
@@ -174,7 +174,7 @@ module "backend_container_service" {
     module.object_storage,
     module.backend_image_registry,
     module.projection_store,
-    module.email
+    #module.email
   ]
 }
 
