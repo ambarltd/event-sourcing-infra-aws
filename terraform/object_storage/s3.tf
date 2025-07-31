b92f6505e8452d5d2f98ac218db33d073ab803e7
Tom Schutte <tom@ambar.cloud>
2025-07-31 15:29:54 -0500

    Add wildcard to CORs domain

--- terraform/object_storage/s3.tf
+++ terraform/object_storage/s3.tf
@@ -14,7 +14,10 @@ resource "aws_s3_bucket_cors_configuration" "cors_configuration" {
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["PUT", "POST", "GET", "HEAD"]
-    allowed_origins = ["https://${var.frontend_cors_domain}"]
+    allowed_origins = [
+      "https://${var.frontend_cors_domain}",
+      "https://*.${var.frontend_cors_domain}",
+    ]
     expose_headers  = ["ETag"]
     max_age_seconds = 3000
   }
