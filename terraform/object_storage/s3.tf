47ee58cf5d61a4661528971c4d39ec67ff3dda43
Tom Schutte <tom@ambar.cloud>
2025-08-14 08:43:38 -0500

    Add public folder to object storage for public resource hosting

--- terraform/object_storage/s3.tf
+++ terraform/object_storage/s3.tf
@@ -61,9 +61,29 @@ resource "aws_s3_bucket_public_access_block" "blob_storage_public_access" {
   bucket = aws_s3_bucket.blob_storage.id
 
   block_public_acls       = true
-  block_public_policy     = true
+  block_public_policy     = false
   ignore_public_acls      = true
-  restrict_public_buckets = true
+  restrict_public_buckets = false
+}
+
+# Resources in the /public/ directory will be accessible without a presigned url
+resource "aws_s3_bucket_policy" "blob_storage_policy" {
+  bucket = aws_s3_bucket.blob_storage.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.blob_storage.arn}/public/*"
+      }
+    ]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.blob_storage_public_access]
 }
 
 # IAM User for S3 bucket access
